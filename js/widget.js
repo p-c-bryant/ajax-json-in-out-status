c23452113a7e3b2f14e0c3c58871e9810a64b4ef
p-c-bryant <63030871+p-c-bryant@users.noreply.github.com>
2020-04-11 13:37:55 -0400

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -1,26 +1,47 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () => {
-  if(xhr.readyState === 4) {
+  if(xhr.readyState === 4 && xhr.status === 200) {
     var employees = JSON.parse(xhr.responseText);
-    
-    var statusUl = '<ul class="bulleted">';
-    
-    for (let i = 0; i < employees.length; i++) {
-      
-      if (employees[i].inoffice) {
-        statusUl += '<li class="in">';
+    var statusHTML = '<ul class="bulleted">';
+    for (var i=0; i<employees.length; i += 1) {
+      if (employees[i].inoffice === true) {
+        statusHTML += '<li class="in">';
       } else {
-        statusUl += '<li class="out">';
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML;
+  }
+};
+xhr.open('GET', '../data/employees.json');
+xhr.send();
+
+
+var confRequest = new XMLHttpRequest();
+confRequest.onreadystatechange = () => {
+  if(confRequest.readyState === 4 && confRequest.status === 200) {
+    var rooms = JSON.parse(confRequest.responseText);
+    var statusHTML = '<ul class="rooms">';
+    
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].available) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
       }
       
-      statusUl += `${employees[i].name}</li>`;
+      statusHTML += `${rooms[i].room}</li>`;
     }
     
-    statusUl += '</ul>';
+    statusHTML += '</ul>';
+    console.log(statusHTML);
+    
+    document.getElementById('roomList').innerHTML = statusHTML;
   }
-  
-  document.getElementById('employeeList').innerHTML = statusUl;
-  
-};
-xhr.open('GET', 'data/employees.json');
-xhr.send();
+}
+confRequest.open('GET', '../data/rooms.json');
+confRequest.send();
+
